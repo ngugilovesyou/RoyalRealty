288446a9c70235e53534849017d3170b1ff89e87
ngugilovesyou <amsamgitau@gmail.com>
2026-07-06 08:18:29 -0400

    new commit

--- client/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://royalrealty.co.ke',
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  changefreq: 'daily',
-  priority: 0.7,
-};
